de0eaf1a867e11f0cc6c4383583e97d4d8d026f1
heliobei <suporte@beieducacaopedagogico.com>
2025-06-09 13:59:34 -0300

    Update ChapterList.jsx

--- src/components/ChapterList.jsx
+++ src/components/ChapterList.jsx
@@ -4,6 +4,7 @@ import ChapterCard from "./ChapterCard";
 const chapters = [
   { id: 1, title: "Como consumir de maneira sustentável?", teacherPdf: "/assets/pdfs/Educador_Capitulo_01_8o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_01_8o.pdf" },
   { id: 2, title: "Como não gastar mais do que deveríamos?", teacherPdf: "/assets/pdfs/Educador_Capitulo_02_8o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_02_8o.pdf" },
+  { id: 3, title: "", teacherPdf: "/assets/pdfs/Educador_Capitulo_03_8o.pdf", studentPdf: "/assets/pdfs/Estudante_Capitulo_03_8o.pdf" },
   
   // ... mais capítulos
 ];
